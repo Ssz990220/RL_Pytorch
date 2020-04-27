4bf19a08568fdd1c5f27053f1e15fc388a97dcc4
Ssz990220 <39759334+Ssz990220@users.noreply.github.com>
2020-04-27 01:27:49 -0700

    Actor_Critic with different env

--- PPO_Single_Thread.py
+++ PPO_Single_Thread.py
@@ -4,12 +4,9 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import math
-from torch.distributions import Categorical
+from torch.distributions import MultivariateNormal
 
 env = gym.make('LunarLander-v2')
-# env = gym.make('MountainCar-v0')
-# env = gym.make('CartPole-v0')
-# env.unwrapped
 state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
 LR = 1e-3
@@ -24,31 +21,27 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.fc1 = nn.Linear(state_size, 16)
         self.fc2 = nn.Linear(16, 16)
-        self.fc3 = nn.Linear(16, action_size)
-        # self.mu = nn.Linear(16, action_size)
-        # self.sigma = nn.Linear(16, action_size)
+        # self.fc3 = nn.Linear(16, action_size)
+        self.mu = nn.Linear(16, action_size)
+        self.sigma = nn.Linear(16, action_size)
         self.optimizer = optim.Adam(self.parameters(), lr=1e-3)
 
     def forward(self, state):
         x = self.fc1(state)
         x = self.fc2(x)
-        # mu = torch.tanh(self.mu(x))
-        # sigma = F.softplus(self.sigma(x))
-        x = self.fc3(x)
-        return F.softmax(x, dim=-1)
+        mu = torch.tanh(self.mu(x))
+        sigma = F.softplus(self.sigma(x))
+        # x = self.fc3(x)
+        return mu, sigma
 
     def get_action(self, state):
         with torch.no_grad():
-            act_prob = self.forward(state)
-            dist = Categorical(act_prob)
-            action = dist.sample().item()
-        return action
-
-    def get_log_prob(self, state, action):
-        act_prob = self.forward(state)
-        dist = Categorical(act_prob)
-        log_prob = dist.log_prob(action)
-        return log_prob
+            mu, sigma = self.forward(state)
+            sigma = torch.diag(sigma)
+            dist = MultivariateNormal(mu, sigma)
+            action = dist.sample()
+            log_prob = dist.log_prob(action)
+        return action, log_prob
 
     def learn(self, s, a, td):
         loss = -(self.get_log_prob(s, a)*td).mean()
@@ -73,14 +66,14 @@ class Critic(nn.Module):
         return value
 
     def learn(self, s, r, s_):
-        td_error = r + GAMMA * self.forward(s_).detach() - self.forward(s)
+        td_error = r + GAMMA * self.forward(s_) - self.forward(s)
         loss = td_error * td_error
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
         return td_error
 
-
+target_actor = Actor(state_size,action_size)
 actor = Actor(state_size, action_size)
 critic = Critic(state_size)
 
@@ -89,7 +82,7 @@ for ep in range(MAX_EPISODE):
     step_count = 0
     ep_rs = 0
     while True:
-        env.render()
+        # env.render()
         action = actor.get_action(torch.as_tensor(state, dtype=torch.float32).unsqueeze(0))
         next_state, reward, done, _ = env.step(action)
         # reward = reward / 10  # Morvan Zhou
--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## DQN note
 * `env.unwrapped` will release the step limit of 200 in 'CartPole-v0' env
 * Network structure matters
+* CNN version is pretty slow, not know the reason yet
+
+## Policy based method
+* Policy Gradient

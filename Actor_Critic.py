0f05816b5ef2fea651e731393235260dcc7ca6f3
Ssz990220 <39759334+Ssz990220@users.noreply.github.com>
2020-04-26 21:01:50 -0700

    Actor_Critic finished

--- Actor_Critic.py
+++ Actor_Critic.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import math
-from torch.distributions import Normal
+from torch.distributions import Categorical
 
-env = gym.make('Pendulum-v0')
+env = gym.make('LunarLander-v2')
 state_size = env.observation_space.shape[0]
-action_size = env.action_space.shape[0]
+action_size = env.action_space.n
 LR = 1e-3
 MAX_EPISODE = 500
 MAX_STEP_EPISODE = 200
@@ -21,31 +21,34 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.fc1 = nn.Linear(state_size, 16)
         self.fc2 = nn.Linear(16, 16)
-        self.mu = nn.Linear(16, action_size)
-        self.sigma = nn.Linear(16, action_size)
-        self.distribution = Normal
+        self.fc3 = nn.Linear(16, action_size)
+        # self.mu = nn.Linear(16, action_size)
+        # self.sigma = nn.Linear(16, action_size)
         self.optimizer = optim.Adam(self.parameters(), lr=1e-3)
 
     def forward(self, state):
-        x = torch.tanh(self.fc1(state))
-        x = torch.tanh(self.fc2(x))
-        mu = torch.tanh(self.mu(x))
-        sigma = F.softplus(self.sigma(x))
-        return mu, sigma
+        x = self.fc1(state)
+        x = self.fc2(x)
+        # mu = torch.tanh(self.mu(x))
+        # sigma = F.softplus(self.sigma(x))
+        x = self.fc3(x)
+        return F.softmax(x, dim=-1)
 
     def get_action(self, state):
         with torch.no_grad():
-            mu, sigma = self.forward(state)
-            m = self.distribution(mu.view(1, ).data, sigma.view(1, ).data)
-            return m.sample().numpy()
+            act_prob = self.forward(state)
+            dist = Categorical(act_prob)
+            action = dist.sample().item()
+        return action
+
+    def get_log_prob(self, state, action):
+        act_prob = self.forward(state)
+        dist = Categorical(act_prob)
+        log_prob = dist.log_prob(action)
+        return log_prob
 
     def learn(self, s, a, td):
-        self.train()
-        mu, sigma = self.forward(s)
-        m = self.distribution(mu.view(1, ).data, sigma.view(1, ).data)
-        log_prob = m.log_prob(a)
-        entropy = 0.5 + 0.5 * torch.log(torch.tensor(2 * math.pi)) + torch.log(m.scale)  # Morvan Zhou
-        loss = log_prob * td.detach() + 0.005 * entropy
+        loss = -(self.get_log_prob(s, a)*td).mean()
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -84,17 +87,17 @@ for ep in range(MAX_EPISODE):
     ep_rs = 0
     while True:
         env.render()
-        action = actor.get_action(torch.as_tensor(state, dtype=torch.float32))
+        action = actor.get_action(torch.as_tensor(state, dtype=torch.float32).unsqueeze(0))
         next_state, reward, done, _ = env.step(action)
-        reward = reward / 10  # Morvan Zhou
+        # reward = reward / 10  # Morvan Zhou
         td_error = critic.learn(torch.as_tensor(state, dtype=torch.float32),
                                 torch.as_tensor(reward, dtype=torch.float32),
                                 torch.as_tensor(next_state, dtype=torch.float32))
         actor.learn(torch.as_tensor(state, dtype=torch.float32),
-                    torch.as_tensor(action, dtype=torch.float32), td_error)
+                    torch.as_tensor(action, dtype=torch.float32), td_error.detach())
         state = next_state
         step_count += 1
         ep_rs += reward
-        if state_size > MAX_STEP_EPISODE:
+        if state_size > MAX_STEP_EPISODE or done:
             print('Episode: ', ep + 1, ' reward is:', int(ep_rs))
             break
